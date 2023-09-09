6d7f6c8f3ea31f75ef91d9164214088ce13a2a11
Brr0 <risa6550@seznam.cz>
2023-09-09 14:27:05 +0200

    first commit

--- settings.tsx
+++ settings.tsx
@@ -1,11 +1,12 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "free", mintText: "Free mint for Z.O.M.B.I.E holders", buttonLabel: "Mint now!", header: "Free Mint" },
-  { label: "wl", mintText: "Discounted mint with a WL token", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "pub", mintText: "Public mint for 0.8SOL", buttonLabel: "Mint now!", header: "Public Mint" },
- // { label: "default", mintText: "Mint with a WL token for 480 USDC", buttonLabel: "Mint now!", header: "WL mint"}
-];
+  { label: "1", mintText: "No token needed", buttonLabel: "Mint now!", header: "Public mint" },
+  { label: "2", mintText: "0.6sol mint token needed", buttonLabel: "Mint now!", header: "0.6sol Mint" },
+  { label: "3", mintText: "0.5sol mint token needed", buttonLabel: "Mint now!", header: "0.5sol Mint" },
+  { label: "4", mintText: "0.4sol mint token needed", buttonLabel: "Mint now!", header: "0.4sol Mint" },
+  { label: "5", mintText: "free mint token needed", buttonLabel: "Mint now!", header: "Free mint" },
+ ];
 
 //header image in the ui. replace with your own
 export const image = "https://xjarh43exldxgj2o7uxeg63kupfwzyelyatns33rnunrv3ndbroa.arweave.net/ukET82S6x3MnTv0uQ3tqo8ts4IvAJtlvcW0bGu2jDFw"; 
